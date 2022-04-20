266e4fccee5401e216cec8b150665e9e863ce434
LiseRochat <liserochat@live.fr>
2022-04-20 13:34:17 +0200

    Correct errors on .htaccess file

--- index.php
+++ index.php
@@ -9,8 +9,9 @@ if(empty($_GET['page'])) {
     On sépare la chaine de carcatere de l'url : /produits/casquettes/ avec la fonction explode() de php
     On obtien un tableau contenant les requetes de l'utilisateur tab[1]=produits tab[2]=casquettes
     */
-    $url = explode("/", filter_var($_Get['page'], FILTER_SANITIZE_URL));
+    $url = explode("/", filter_var($_GET['page'], FILTER_SANITIZE_URL));
     $page = $url[0];
+    print_r($url);
 }
 
 // On gère le premier niveau d'url
@@ -20,13 +21,13 @@ switch($page) {
         // Declaration des variables 
         $page_description = "Strucuture de base d'un projet en php";
         $page_title = "Projet PHP MVC";
-        $page_content = "Bonjour Tous le monde !";
+        $page_content = "<h1>Bonjour Tous le monde !</h1>";
     break; 
     case "page1" :
         // Declaration des variables 
         $page_description = "Strucuture de base d'un projet en php";
         $page_title = "Projet PHP MVC";
-        $page_content = "Bonjour Tous le monde Page 1 !";
+        $page_content = "<h1>Bonjour Tous le monde Page 1 !</h1>";
     break; 
 }
 
