788609669536998523c42f44ae38b932d5a6fc60
LiseRochat <liserochat@live.fr>
2022-04-20 10:26:35 +0200

    Add php variables on template.php

--- Views/Common/template.php
+++ Views/Common/template.php
@@ -4,10 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <title>Document</title>
+    <!-- description spécifique à chaque page -->
+    <meta name="description" content="<?php echo $page_description; ?>">
+    <!-- Title spécifique à chaque page -->
+    <title><?php echo $page_title; ?></title>
 </head>
 <body>
-    
+    <!-- page_content : contenu des differentes pages -->
+    <?php echo $page_content; ?>
 </body>
 </html>
