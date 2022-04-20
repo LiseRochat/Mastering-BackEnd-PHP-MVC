61b0a5fa786cfcf6812a8a2653e26c16536e13df
LiseRochat <liserochat@live.fr>
2022-04-20 20:33:22 +0200

    Creations all views of project

--- Controllers/MainController.php
+++ Controllers/MainController.php
@@ -17,14 +17,18 @@ class Main {
     public function page1() {
         $page_description = "Strucuture de base d'un projet en php";
         $page_title = "Projet PHP MVC";
-        $page_content = "<h1>Bonjour Tous le monde Page 1 !</h1>";
+        ob_start();
+        require_once("./Views/page1.php");
+        $page_content = ob_get_clean();
         require("Views/Common/template.php");
     }
 
     public function pageErrors($message) {
         $page_description = "Page permettant de gérer les erreurs";
         $page_title = "Page d'erreur";
-        $page_content = $message;
+        ob_start();
+        require_once("./Views/errors.php");
+        $page_content = ob_get_clean();
         require("Views/Common/template.php");
     }
 }
