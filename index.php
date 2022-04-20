169a24b5e03cb7ccedf95c78e4cd6108f20050ff
LiseRochat <liserochat@live.fr>
2022-04-20 12:48:33 +0200

    Use explode() to manage url

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php 
 
+// Test si l'information index.php?page= est vide 
+if(empty($_GET['page'])) {
+
+} else {
+    /*
+    On sépare la chaine de carcatere de l'url : /produits/casquettes/ avec la fonction explode() de php
+    On obtien un tableau contenant les requetes de l'utilisateur tab[1]=produits tab[2]=casquettes
+    */
+    $page = explode("/", $_Get['page']);
+}
 // Declaration des variables 
 $page_description = "Strucuture de base d'un projet en php";
 $page_title = "Projet PHP MVC";
