2b3d98278de2b55b63a10ca00be777b66751a66f
LiseRochat <liserochat@live.fr>
2022-04-20 21:13:00 +0200

    Create function getData

--- Models/MainManager.php
+++ Models/MainManager.php
@@ -1,5 +1,15 @@
 <?php 
 // Ne sert que à recupérer les données de la bdd
 class MainManager {
-    
+    /*
+    * Fonction permettant de simuler une récupération de données
+    * depuis une base de données ou autre
+    */
+    public function getData() {
+        $data = [
+            "data1" => "a",
+            "data2" => "b",
+        ];
+        return $data;
+    }
 }
