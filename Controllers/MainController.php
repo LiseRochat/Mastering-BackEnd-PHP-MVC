d64e653800cf32a358ffcd62f761363ae37578fe
LiseRochat <liserochat@live.fr>
2022-04-20 15:13:04 +0200

    Create Main Controller object

---
+++ Controllers/MainController.php
@@ -0,0 +1,18 @@
+<?php 
+/* 
+    Création d'un objet Main
+    Une fonction par demande de l'utilisateur
+*/
+class Main {
+    
+    public function home() {
+
+    }
+
+    public function page1() {
+
+    }
+    public function pageErrors() {
+
+    }
+}
