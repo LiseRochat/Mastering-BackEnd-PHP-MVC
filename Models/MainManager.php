a324586c2f49d546a14a8bd018a885aec1d9cce6
LiseRochat <liserochat@live.fr>
2022-04-20 22:57:12 +0200

    Update MainManager file

--- Models/MainManager.php
+++ Models/MainManager.php
@@ -1,15 +1,22 @@
 <?php 
+
+require_once("./Controllers/BDDClass.php");
 // Ne sert que à recupérer les données de la bdd
-class MainManager {
+class MainManager extends BDD {
     /*
     * Fonction permettant de simuler une récupération de données
     * depuis une base de données ou autre
     */
-    public function getDataX() {
-        $data = [
-            "data1" => "a",
-            "data2" => "b",
-        ];
-        return $data;
+    public function getDatas() {
+        // Je recupere la connexion a la base de données
+        $pdo = $this->getBdd();
+        // Je prepare la requete sql
+        $req = $pdo->prepare("SELECT * FROM exemple ");
+        // Je lance la requete
+        $req->execute();
+        // Je traite les données : FETCH_ASSOC permet d'éviter d'avoir deux fois les informations retournées
+        $datas = $req->fetchAll(PDO::FETCH_ASSOC);
+        // Je retourne les informations
+        return $datas;
     }
 }
